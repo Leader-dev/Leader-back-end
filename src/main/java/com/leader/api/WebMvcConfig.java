ac102e10eb6957e10acf7b354520baf39a8e82b9
wuhaozhen <wuhaozhen1106@gmail.com>
2021-05-19 03:17:37 +0800

    Add automatic conversion config from ObjectId to String in response

--- src/main/java/com/leader/api/WebMvcConfig.java
+++ src/main/java/com/leader/api/WebMvcConfig.java
@@ -1,15 +1,23 @@
 package com.leader.api;
 
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.bson.types.ObjectId;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Configuration
+@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private final String AUTH_ERROR_RESPONSE = "{\"code\":403}";
@@ -32,5 +40,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 })
                 .addPathPatterns("/**")
                 .excludePathPatterns("/user/**");
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder()
+            .serializerByType(ObjectId.class, new ToStringSerializer());
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter(builder.build());
+        converters.add(converter);
     }
 }

fad51ddd25df8eaf91c9a2b71a61eeb85f88b49b
wuhaozhen <wuhaozhen1106@gmail.com>
2021-05-25 14:40:28 +0800

    Reconstruct SecureUtil for greater stability

--- src/main/java/com/leader/api/util/SecureUtil.java
+++ src/main/java/com/leader/api/util/SecureUtil.java
@@ -8,6 +8,9 @@ import java.util.Base64;
 public class SecureUtil {
 
     private final static String CHARSET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcsdefghijklmnopqrstuvwxyz0123456789";
+    private final static String SHA1_MESSAGE_DIGEST_ALGORITHM_NAME = "SHA-1";
+    private final static String RSA_KEY_PAIR_GENERATOR_ALGORITHM_NAME = "RSA";
+    private final static String RSA_CIPHER_TRANSFORMATION_NAME = "RSA/ECB/PKCS1Padding";
 
     public static String createRandomSalt(int length) {
         StringBuilder saltBuilder = new StringBuilder(length);
@@ -22,7 +25,7 @@ public class SecureUtil {
 
     public static String SHA1(String message) {
         try {
-            MessageDigest messageDigest = MessageDigest.getInstance("SHA-1");
+            MessageDigest messageDigest = MessageDigest.getInstance(SHA1_MESSAGE_DIGEST_ALGORITHM_NAME);
             messageDigest.update(message.getBytes());
             return new String(messageDigest.digest());
         } catch (NoSuchAlgorithmException e) {
@@ -49,7 +52,7 @@ public class SecureUtil {
 
     public static KeyPair generateRSAKeyPair(int keysize) {
         try {
-            KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+            KeyPairGenerator generator = KeyPairGenerator.getInstance(RSA_KEY_PAIR_GENERATOR_ALGORITHM_NAME);
             generator.initialize(keysize);
             return generator.generateKeyPair();
         } catch (NoSuchAlgorithmException e) {
@@ -60,7 +63,7 @@ public class SecureUtil {
 
     public static String decryptRSA(String cipherText, PrivateKey key) {
         try {
-            Cipher cipher = Cipher.getInstance("RSA");
+            Cipher cipher = Cipher.getInstance(RSA_CIPHER_TRANSFORMATION_NAME);
             cipher.init(Cipher.DECRYPT_MODE, key);
             byte[] bytesIn = cipherText.getBytes(StandardCharsets.UTF_8);
             byte[] bytesDecoded = Base64.getDecoder().decode(bytesIn);
